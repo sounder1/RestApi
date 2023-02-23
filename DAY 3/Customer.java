16744f41100e07502697f72dc8fc2843f8c5d06e
sounder1 <117268856+sounder1@users.noreply.github.com>
2023-02-23 12:28:04 +0530

    REST API USING SPRING BOOT
    SOUNDER THE MA

---
+++ DAY 3/Customer.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+	@Id
+	private int id;
+	private String name;
+	private int age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	
+}
---
+++ DAY 3/CustomerController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Customer;
+import com.example.demo.service.CustomerService;
+
+@RestController
+public class CustomerController {
+		@Autowired
+		CustomerService service;
+		
+		@PostMapping("/customer")
+		public String add(@RequestBody Customer customer) {
+			return service.addCustomer(customer);
+		}
+		
+		@GetMapping("/customer")
+		public List<Customer> get(){
+			return service.getCustomer();
+		}
+
+}
